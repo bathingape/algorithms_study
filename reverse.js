f5e147a46071b462ab8e7a198f913427f358aa0c
ash-neopons <ash@neopons.com>
2021-07-20 16:47:57 +0900

    reverse 알고리즘 문제 및 첫 커밋

---
+++ reverse.js
@@ -0,0 +1,16 @@
+// --- Examples
+//   reverse('apple') === 'elppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+function reverse(str) {
+    let result = '';
+
+    for (let i = 1; i <= str.length; i++) {
+        // 가장 뒤에 있는 문자열을 가장 첫번째로 넣는다.
+        // ex) apple -> 4번째 idx 'e'를 리턴할 문자열 idx 0으로 보내야함.
+        result += str[str.length - i];
+    }
+    return result;
+}
+
+console.log(reverse('Greetings!'));
